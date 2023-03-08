892093231ac831e212fdfc7b40d28dfccf4a4dca
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-03-08 15:02:53 +0200

    edit
    edit

--- src/components/EditForm/EditForm.styled.jsx
+++ src/components/EditForm/EditForm.styled.jsx
@@ -33,9 +33,10 @@ export const CustomError = styled.div`
   top: 0;
   left: 100%;
   padding: 3px;
-  width: 110px;
+  width: 130px;
   border: 1px solid red;
   border-radius: 4px;
   color: red;
-  font-size: 8px;
+  font-size: 10px;
+  font-weight: 700;
 `;
